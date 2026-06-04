fb7677574f1f8d9674bf9ef79515fd8a108f02f1
ganjangbibimbab <swchoi0731@suwon.ac.kr>
2026-06-04 15:08:33 +0900

    테에스트 이티시

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            fontDialog1 = new FontDialog();
+            dataGridView1 = new DataGridView();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            fontDialog2 = new FontDialog();
+            etc = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(178, 145);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(360, 225);
+            dataGridView1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Location = new Point(629, 92);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(8, 8);
+            flowLayoutPanel1.TabIndex = 1;
+            // 
+            // etc
+            // 
+            etc.Location = new Point(62, 84);
+            etc.Name = "etc";
+            etc.Size = new Size(112, 34);
+            etc.TabIndex = 2;
+            etc.Text = "etc";
+            etc.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(etc);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private FontDialog fontDialog1;
+        private DataGridView dataGridView1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private FontDialog fontDialog2;
+        private Button etc;
     }
 }
